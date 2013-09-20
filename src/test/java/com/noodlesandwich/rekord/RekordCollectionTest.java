9cda1bba0342ee464fd61cfc4dcfe258212d6564
Samir Talwar <samir@noodlesandwich.com>
2013-09-20 13:32:06 +0100

    Moar static imports.

--- src/test/java/com/noodlesandwich/rekord/RekordCollectionTest.java
+++ src/test/java/com/noodlesandwich/rekord/RekordCollectionTest.java
@@ -5,7 +5,6 @@ import org.jmock.Mockery;
 import org.jmock.Sequence;
 import org.junit.Test;
 import com.noodlesandwich.rekord.testobjects.Measurement;
-import com.noodlesandwich.rekord.testobjects.Rekords;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.allOf;
@@ -13,6 +12,8 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasToString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
+import static com.noodlesandwich.rekord.testobjects.Rekords.Bier;
+import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich;
 import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich.Bread.White;
 import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich.Filling.Cheese;
 import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich.Style.Burger;
@@ -22,21 +23,21 @@ public final class RekordCollectionTest {
 
     @Test public void
     a_Rekord_can_be_collected_into_a_collector() {
-        Rekord<Rekords.Sandvich> sandvich = Rekord.of(Rekords.Sandvich.class)
-                .with(Rekords.Sandvich.filling, Cheese)
-                .with(Rekords.Sandvich.bread, White)
-                .with(Rekords.Sandvich.style, Burger);
+        Rekord<Sandvich> sandvich = Rekord.of(Sandvich.class)
+                .with(Sandvich.filling, Cheese)
+                .with(Sandvich.bread, White)
+                .with(Sandvich.style, Burger);
 
         @SuppressWarnings("unchecked")
-        final RekordCollector<Rekords.Sandvich, String> collector = context.mock(RekordCollector.class);
+        final RekordCollector<Sandvich, String> collector = context.mock(RekordCollector.class);
         final String expectedResult = "result!";
         context.checking(new Expectations() {{
             Sequence filling = context.sequence("filling");
             Sequence bread = context.sequence("bread");
             Sequence style = context.sequence("style");
-            oneOf(collector).accumulate(Rekords.Sandvich.filling, Cheese); inSequence(filling);
-            oneOf(collector).accumulate(Rekords.Sandvich.bread, White); inSequence(bread);
-            oneOf(collector).accumulate(Rekords.Sandvich.style, Burger); inSequence(style);
+            oneOf(collector).accumulate(Sandvich.filling, Cheese); inSequence(filling);
+            oneOf(collector).accumulate(Sandvich.bread, White); inSequence(bread);
+            oneOf(collector).accumulate(Sandvich.style, Burger); inSequence(style);
             oneOf(collector).result(); will(returnValue(expectedResult)); inSequences(filling, bread, style);
         }});
 
@@ -48,9 +49,9 @@ public final class RekordCollectionTest {
 
     @Test public void
     a_Rekord_is_serializable_as_a_String() {
-        Rekord<Rekords.Bier> delicious = Rekord.of(Rekords.Bier.class)
-                                       .with(Rekords.Bier.volume, Measurement.of(568).ml())
-                                       .with(Rekords.Bier.head, Measurement.of(3).cm());
+        Rekord<Bier> delicious = Rekord.of(Bier.class)
+                                       .with(Bier.volume, Measurement.of(568).ml())
+                                       .with(Bier.head, Measurement.of(3).cm());
 
         assertThat(delicious, hasToString(allOf(startsWith("Bier"), containsString("head=3cm"), containsString("volume=568ml"))));
     }
