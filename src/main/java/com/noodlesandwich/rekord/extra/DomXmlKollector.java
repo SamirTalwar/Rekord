4ef8af959ce5181ee8bfd0e7c5b51bbf358ffcc0
Samir Talwar <stalwar@palantir.com>
2013-12-30 15:37:26 +0000

    Rekord and key names are lowercased and slugified when generating XML.

--- src/main/java/com/noodlesandwich/rekord/extra/DomXmlKollector.java
+++ src/main/java/com/noodlesandwich/rekord/extra/DomXmlKollector.java
@@ -1,10 +1,12 @@
 package com.noodlesandwich.rekord.extra;
 
+import java.util.regex.Pattern;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import com.google.common.base.Joiner;
 import com.noodlesandwich.rekord.Key;
 import com.noodlesandwich.rekord.Kollector;
 
@@ -32,13 +34,13 @@ public final class DomXmlKollector implements Kollector<Document> {
 
         public XmlAccumulator(String name, Document document) {
             this.document = document;
-            this.root = document.createElement(name);
+            this.root = document.createElement(slugify(name));
             document.appendChild(root);
         }
 
         @Override
         public <V> void accumulate(Key<?, V> key, V value) {
-            Element element = document.createElement(key.toString());
+            Element element = document.createElement(slugify(key.toString()));
             element.appendChild(document.createTextNode(value.toString()));
             root.appendChild(element);
         }
@@ -48,4 +50,42 @@ public final class DomXmlKollector implements Kollector<Document> {
             return document;
         }
     }
+
+    public static String slugify(String name) {
+        name = name.replaceAll(" ", "-");
+        name = InvalidXmlNameChars.matcher(name).replaceAll("_");
+        if (InvalidXmlNameStartChars.matcher(name.substring(0, 1)).matches()) {
+            name = "_" + name;
+        }
+        return name.toLowerCase();
+    }
+
+    private static final String XmlNameStartCharsString = Joiner.on("").join(
+            ":", "_",
+            "A-Z", "a-z",
+            "\u00c0-\u00d6", "\u00d8-\u00f6", "\u00f8-\u02ff", "\u0370-\u037D",
+            "\u037F-\u1FFF", "\u200C-\u200D", "\u2070-\u218F", "\u2C00-\u2FEF",
+            "\u3001-\uD7FF", "\uF900-\uFDCF", "\uFDF0-\uFFFD"
+    );
+    private static final Pattern InvalidXmlNameStartChars = Pattern.compile("[^" + XmlNameStartCharsString + "]");
+
+    private static final String XmlNameCharsString = Joiner.on("").join(
+            XmlNameStartCharsString,
+            "\\-", "\\.", "0-9", "\u00b7", "\u0300-\u036f", "\u203f-\u2040"
+    );
+    private static final Pattern InvalidXmlNameChars = Pattern.compile("[^" + XmlNameCharsString + "]");
+
+    /*
+    NameStartChar   ::=
+        ":" | [A-Z] | "_" | [a-z] | [#xC0-#xD6] | [#xD8-#xF6] | [#xF8-#x2FF] |
+        [#x370-#x37D] | [#x37F-#x1FFF] | [#x200C-#x200D] | [#x2070-#x218F] |
+        [#x2C00-#x2FEF] | [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] |
+        [#x10000-#xEFFFF]
+
+    NameChar    ::=
+        NameStartChar | "-" | "." | [0-9] | #xB7 | [#x0300-#x036F] | [#x203F-#x2040]
+
+    Name    ::=
+        NameStartChar (NameChar)*
+     */
 }
--- src/test/java/com/noodlesandwich/rekord/extra/DomXmlKollectorTest.java
+++ src/test/java/com/noodlesandwich/rekord/extra/DomXmlKollectorTest.java
@@ -3,11 +3,13 @@ package com.noodlesandwich.rekord.extra;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import com.google.common.base.Joiner;
+import com.noodlesandwich.rekord.Key;
 import com.noodlesandwich.rekord.Rekord;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.xmlmatchers.XmlMatchers.isSimilarTo;
 import static org.xmlmatchers.transform.XmlConverters.the;
+import static com.noodlesandwich.rekord.testobjects.Rekords.Person;
 import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich;
 import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich.Bread.Brown;
 import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich.Filling.Cheese;
@@ -44,6 +46,29 @@ public final class DomXmlKollectorTest {
                 "    <filling>Cheese</filling>",
                 "    <style>Roll</style>",
                 "</sandvich>"
+        ))));
+    }
+
+    @Test public void
+    rekord_and_key_names_are_lowercased_and_slugified_into_valid_XML_element_names() {
+        Rekord<Person> spongebob = Rekord.of(Person.class)
+                .with(Person.firstName, "Spongebob")
+                .with(Person.lastName, "Squarepants")
+                .with(Person.age, 27)
+                .with(Key.<Person, Integer>named("12345"), 67890)
+                .with(Key.<Person, String>named("^up"), "down");
+
+        Document document = spongebob.collect(new DomXmlKollector("sea creaTURE"));
+
+        assertThat(the(document), isSimilarTo(the(lines(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
+                "<sea-creature>",
+                "    <first-name>Spongebob</first-name>",
+                "    <last-name>Squarepants</last-name>",
+                "    <age>27</age>",
+                "    <_12345>67890</_12345>",
+                "    <_up>down</_up>",
+                "</sea-creature>"
         ))));
     }
 
