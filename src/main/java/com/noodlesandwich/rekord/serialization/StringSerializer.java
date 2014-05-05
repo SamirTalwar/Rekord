7fd3ffdfe788a8288467cc05c8aff7ea855d5e56
Samir Talwar <samir@noodlesandwich.com>
2014-05-05 12:44:22 +0100

    Extract out the separator duplication in the string accumulators.

--- src/main/java/com/noodlesandwich/rekord/serialization/StringSerializer.java
+++ src/main/java/com/noodlesandwich/rekord/serialization/StringSerializer.java
@@ -11,10 +11,51 @@ public final class StringSerializer implements Serializer<String, String> {
         return accumulator.serialized();
     }
 
+    private static final class SingleStringProperty implements SerializedProperty<String> {
+        private final String value;
+
+        public SingleStringProperty(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String serialized() {
+            return value;
+        }
+    }
+
+    private static final class StringCollectionAccumulator implements Accumulator<String> {
+        private final DelimitedString builder = new DelimitedString();
+
+        @Override
+        public SerializedProperty<String> single(String name, Object value) {
+            return new SingleStringProperty(value.toString());
+        }
+
+        @Override
+        public Accumulator<String> collection(String name) {
+            return new StringCollectionAccumulator();
+        }
+
+        @Override
+        public Accumulator<String> nest(String name) {
+            return new StringAccumulator(name);
+        }
+
+        @Override
+        public void accumulate(String name, SerializedProperty<String> property) {
+            builder.add(property.serialized());
+        }
+
+        @Override
+        public String serialized() {
+            return String.format("[%s]", builder.toString());
+        }
+    }
+
     private static final class StringAccumulator implements Serializer.Accumulator<String> {
         private final String name;
-        private final StringBuilder entries = new StringBuilder();
-        private boolean first = true;
+        private final DelimitedString builder = new DelimitedString();
 
         public StringAccumulator(String name) {
             this.name = name;
@@ -37,67 +78,27 @@ public final class StringSerializer implements Serializer<String, String> {
 
         @Override
         public void accumulate(String name, SerializedProperty<String> property) {
-            appendSeparator();
-            entries.append(name)
-                   .append(": ")
-                   .append(property.serialized());
+            builder.add(String.format("%s: %s", name, property.serialized()));
         }
 
         @Override
         public String serialized() {
-            return String.format("%s {%s}", name, entries.toString());
-        }
-
-        private void appendSeparator() {
-            if (!first) {
-                entries.append(", ");
-            } else {
-                first = false;
-            }
+            return String.format("%s {%s}", name, builder.toString());
         }
     }
 
-    private static final class SingleStringProperty implements SerializedProperty<String> {
-        private final String value;
-
-        public SingleStringProperty(String value) {
-            this.value = value;
-        }
-
-        @Override
-        public String serialized() {
-            return value;
-        }
-    }
-
-    private static final class StringCollectionAccumulator implements Accumulator<String> {
+    private static final class DelimitedString {
         private final StringBuilder entries = new StringBuilder();
         private boolean first = true;
 
-        @Override
-        public SerializedProperty<String> single(String name, Object value) {
-            return new SingleStringProperty(value.toString());
-        }
-
-        @Override
-        public Accumulator<String> collection(String name) {
-            return new StringCollectionAccumulator();
-        }
-
-        @Override
-        public Accumulator<String> nest(String name) {
-            return new StringAccumulator(name);
-        }
-
-        @Override
-        public void accumulate(String name, SerializedProperty<String> property) {
+        public void add(String string) {
             appendSeparator();
-            entries.append(property.serialized());
+            entries.append(string);
         }
 
         @Override
-        public String serialized() {
-            return String.format("[%s]", entries.toString());
+        public String toString() {
+            return entries.toString();
         }
 
         private void appendSeparator() {
