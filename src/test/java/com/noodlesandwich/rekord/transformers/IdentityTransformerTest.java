6c43bf38a922809a0926386d40f7380f96206d18
Samir Talwar <samir@noodlesandwich.com>
2014-05-08 23:55:53 +0100

    Test the `IdentityTransformer`.

---
+++ src/test/java/com/noodlesandwich/rekord/transformers/IdentityTransformerTest.java
@@ -0,0 +1,14 @@
+package com.noodlesandwich.rekord.transformers;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public final class IdentityTransformerTest {
+    @Test public void
+    transforms_any_input_to_itself() {
+        Transformer<String, String> transformer = new IdentityTransformer<>();
+        assertThat(transformer.transformInput("abc"), is("abc"));
+    }
+}
