269e4edd1ee4e407755dd06d0925ba3f7f8f78d6
Samir Talwar <samir@noodlesandwich.com>
2014-05-16 21:43:08 +0100

    Reorder methods.

--- src/main/java/com/noodlesandwich/rekord/Key.java
+++ src/main/java/com/noodlesandwich/rekord/Key.java
@@ -5,13 +5,13 @@ import com.noodlesandwich.rekord.serialization.Serializer;
 import com.noodlesandwich.rekord.transformers.Transformer;
 
 public interface Key<T, V> extends Named {
-    <NewV> Key<T, NewV> that(Transformer<V, NewV> transformer);
-
-    <NewV> Key<T, NewV> then(Transformer<V, NewV> transformer);
-
     <P extends T> Properties<P> storeTo(Properties<P> properties, V value);
 
     <P extends T> V retrieveFrom(Properties<P> properties);
+
+    <NewV> Key<T, NewV> that(Transformer<V, NewV> transformer);
+
+    <NewV> Key<T, NewV> then(Transformer<V, NewV> transformer);
 
     <A, E extends Exception> void accumulate(V value, Serializer.Accumulator<A, E> accumulator) throws E;
 
