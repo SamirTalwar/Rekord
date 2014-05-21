6d12738e49cc3468add05625de36f0195da94ecb
Samir Talwar <samir@noodlesandwich.com>
2014-05-21 22:16:46 +0100

    Add `equals`, `hashCode` and `toString` to the property objects.

--- src/main/java/com/noodlesandwich/rekord/properties/SimpleProperty.java
+++ src/main/java/com/noodlesandwich/rekord/properties/SimpleProperty.java
@@ -1,8 +1,11 @@
 package com.noodlesandwich.rekord.properties;
 
+import java.util.Objects;
 import com.noodlesandwich.rekord.keys.Key;
 
 public final class SimpleProperty<T, V> implements Property<T, V> {
+    private static final String FORMAT_STRING = "(%s: %s)";
+
     private final Key<? super T, V> key;
     private final V value;
 
@@ -19,5 +22,31 @@ public final class SimpleProperty<T, V> implements Property<T, V> {
     @Override
     public V value() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof SimpleProperty)) {
+            return false;
+        }
+
+        @SuppressWarnings("unchecked")
+        SimpleProperty<T, V> that = (SimpleProperty<T, V>) o;
+        return key.equals(that.key) && value.equals(that.value);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(FORMAT_STRING, key, value);
     }
 }
--- src/main/java/com/noodlesandwich/rekord/properties/TransformingProperty.java
+++ src/main/java/com/noodlesandwich/rekord/properties/TransformingProperty.java
@@ -1,10 +1,13 @@
 package com.noodlesandwich.rekord.properties;
 
+import java.util.Objects;
 import com.noodlesandwich.rekord.keys.Key;
 import com.noodlesandwich.rekord.keys.TransformingKey;
 import com.noodlesandwich.rekord.transformers.Transformer;
 
 public final class TransformingProperty<T, U, V> implements Property<T, U> {
+    private static final String FORMAT_STRING = "transformation over (%s: %s)";
+
     private final TransformingKey<T, U, V> key;
     private final V value;
     private final Transformer<U, V> transformer;
@@ -23,5 +26,31 @@ public final class TransformingProperty<T, U, V> implements Property<T, U> {
     @Override
     public U value() {
         return transformer.transformInput(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof TransformingProperty)) {
+            return false;
+        }
+
+        @SuppressWarnings("unchecked")
+        TransformingProperty<T, U, V> that = (TransformingProperty<T, U, V>) o;
+        return key.equals(that.key) && value.equals(that.value) && transformer.equals(that.transformer);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value, transformer);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(FORMAT_STRING, key, value);
     }
 }
