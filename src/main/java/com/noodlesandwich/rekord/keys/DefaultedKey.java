10360a50339ade4193c0bdea67fe3e22a5de16ab
Samir Talwar <samir@noodlesandwich.com>
2014-05-29 13:59:11 +0100

    A DefaultedKey delegates to the supplied key.

---
+++ src/main/java/com/noodlesandwich/rekord/keys/DefaultedKey.java
@@ -0,0 +1,51 @@
+package com.noodlesandwich.rekord.keys;
+
+import com.noodlesandwich.rekord.implementation.AbstractKey;
+import com.noodlesandwich.rekord.properties.Property;
+import com.noodlesandwich.rekord.properties.PropertyMap;
+import com.noodlesandwich.rekord.serialization.Serializer;
+
+public final class DefaultedKey<T, V> extends AbstractKey<T, V> {
+    private final Key<T, V> key;
+
+    public DefaultedKey(Key<T, V> key, V defaultValue) {
+        super(key.name());
+        this.key = key;
+    }
+
+    public static <T, V> UnsureDefaultedKey<T, V> wrapping(Key<T, V> key) {
+        return new UnsureDefaultedKey<>(key);
+    }
+
+    @Override
+    public Property<T, V> of(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V get(PropertyMap<? extends T> properties) {
+        return properties.get(key);
+    }
+
+    @Override
+    public boolean test(PropertyMap<? extends T> properties) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <A, E extends Exception> void accumulate(V value, Serializer.Accumulator<A, E> accumulator) throws E {
+        throw new UnsupportedOperationException();
+    }
+
+    public static final class UnsureDefaultedKey<T, V> {
+        private final Key<T, V> key;
+
+        public UnsureDefaultedKey(Key<T, V> key) {
+            this.key = key;
+        }
+
+        public DefaultedKey<T, V> defaultingTo(V defaultValue) {
+            return new DefaultedKey<>(key, defaultValue);
+        }
+    }
+}
---
+++ src/test/java/com/noodlesandwich/rekord/keys/DefaultedKeyTest.java
@@ -0,0 +1,26 @@
+package com.noodlesandwich.rekord.keys;
+
+import com.noodlesandwich.rekord.Rekord;
+import org.junit.Test;
+
+import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich;
+import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich.Bread.White;
+import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich.Style;
+import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich.Style.Flat;
+import static com.noodlesandwich.rekord.testobjects.Rekords.Sandvich.Style.Roll;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public final class DefaultedKeyTest {
+    private static final Key<Sandvich, Style> styleDefaultingToFlat
+            = DefaultedKey.wrapping(Sandvich.style).defaultingTo(Flat);
+
+    @Test public void
+    delegates_to_the_provided_key_if_there_is_a_stored_value() {
+        Rekord<Sandvich> whiteRoll = Sandvich.rekord
+                .with(Sandvich.bread, White)
+                .with(Sandvich.style, Roll);
+
+        assertThat(whiteRoll.get(styleDefaultingToFlat), is(Roll));
+    }
+}
