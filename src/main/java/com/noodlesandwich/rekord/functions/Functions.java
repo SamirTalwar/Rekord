0e4a999585d0ca0aee790257506133cdd4458e7b
Samir Talwar <samir@noodlesandwich.com>
2014-05-31 17:22:46 +0100

    Functions can be composed.

--- src/main/java/com/noodlesandwich/rekord/functions/Functions.java
+++ src/main/java/com/noodlesandwich/rekord/functions/Functions.java
@@ -4,7 +4,8 @@ public final class Functions {
     private Functions() { }
 
     public static <A, B> InvertibleFunction<A, B> invertibleFrom(
-            final Function<A, B> applyForward, final Function<B, A> applyBackward
+            final Function<A, B> applyForward,
+            final Function<B, A> applyBackward
     ) {
         return new InvertibleFunction<A, B>() {
             @Override
@@ -15,6 +16,18 @@ public final class Functions {
             @Override
             public A applyBackward(B input) {
                 return applyBackward.apply(input);
+            }
+        };
+    }
+
+    public static <A, B, C> Function<A, C> compose(
+            final Function<? super B, ? extends C> b,
+            final Function<? super A, ? extends B> a
+    ) {
+        return new Function<A, C>() {
+            @Override
+            public C apply(A input) {
+                return b.apply(a.apply(input));
             }
         };
     }
---
+++ src/test/java/com/noodlesandwich/rekord/functions/FunctionTest.java
@@ -0,0 +1,33 @@
+package com.noodlesandwich.rekord.functions;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public final class FunctionTest {
+    @Test public void
+    can_be_composed() {
+        Function<Integer, String> stringMultiplier = Functions.compose(stringify(), multiplyBy(2));
+
+        assertThat(stringMultiplier.apply(3), is("6"));
+    }
+
+    public static Function<Integer, Integer> multiplyBy(final int multiplicand) {
+        return new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer input) {
+                return input * multiplicand;
+            }
+        };
+    }
+
+    public static Function<Object, String> stringify() {
+        return new Function<Object, String>() {
+            @Override
+            public String apply(Object input) {
+                return input.toString();
+            }
+        };
+    }
+}
