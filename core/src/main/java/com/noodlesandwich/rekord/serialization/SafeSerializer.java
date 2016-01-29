16be8ce8e707d85cb1479f6316b4dc1fd7eb7b1a
Samir Talwar <samir@noodlesandwich.com>
2016-01-29 12:02:58 +0000

    Explicitly override `serialize` in `SafeSerializer`.

--- core/src/main/java/com/noodlesandwich/rekord/serialization/SafeSerializer.java
+++ core/src/main/java/com/noodlesandwich/rekord/serialization/SafeSerializer.java
@@ -1,6 +1,11 @@
 package com.noodlesandwich.rekord.serialization;
 
+import com.noodlesandwich.rekord.FixedRekord;
+
 public interface SafeSerializer<S> extends Serializer<S, SafeSerializer.ImpossibleException> {
+    @Override
+    <T> S serialize(String name, FixedRekord<T> rekord);
+
     interface SafeAccumulator<A> extends Accumulator<A, ImpossibleException> { }
 
     final class ImpossibleException extends RuntimeException {
