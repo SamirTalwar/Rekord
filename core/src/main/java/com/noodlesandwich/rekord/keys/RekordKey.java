1ba57ad94fa9e498bab64d08e74fb127ea7f4c7b
Samir Talwar <samir@noodlesandwich.com>
2016-01-30 18:44:19 +0000

    Don't use the BuildableKeyDecorator for RekordKey. Keep it simple.

--- core/src/main/java/com/noodlesandwich/rekord/keys/RekordKey.java
+++ core/src/main/java/com/noodlesandwich/rekord/keys/RekordKey.java
@@ -4,13 +4,21 @@ import com.noodlesandwich.rekord.Rekord;
 import com.noodlesandwich.rekord.serialization.Serialization;
 import com.noodlesandwich.rekord.serialization.Serializer;
 
-public final class RekordKey<T, V> extends OriginalKey<T, Rekord<V>> {
-    private RekordKey(String name) {
+public final class RekordKey<T, V> extends OriginalKey<T, Rekord<V>> implements BuildableKey<T, Rekord<V>> {
+    private final Rekord<V> builder;
+
+    private RekordKey(String name, Rekord<V> builder) {
         super(name);
+        this.builder = builder;
     }
 
     public static UnbuildableRekordKey named(String name) {
         return new UnbuildableRekordKey(name);
+    }
+
+    @Override
+    public Rekord<V> builder() {
+        return builder;
     }
 
     @Override
@@ -31,7 +39,7 @@ public final class RekordKey<T, V> extends OriginalKey<T, Rekord<V>> {
         }
 
         public <T, V> BuildableKey<T, Rekord<V>> builtFrom(Rekord<V> builder) {
-            return new BuildableKeyDecorator<>(new RekordKey<T, V>(name), builder);
+            return new RekordKey<>(name, builder);
         }
     }
 }
